27240c1dae58ccec3484e2c95f2e754148cd9eb8
Arbaz.Shk <162773130+spiddeeyy-45@users.noreply.github.com>
2026-03-28 18:55:25 +0530

    Day3: Vehicle UI updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation ("com.github.Dimezis:BlurView:version-2.0.3")
 }
